9da7b2c65a3b52a40273b8e0b9b9e4f79741dbd3
A-Aleksandpov <117276628+A-Aleksandpov@users.noreply.github.com>
2023-10-23 10:11:25 +0300

    Update togdrive.py

--- 01.3 data science Gdrive/togdrive.py
+++ 01.3 data science Gdrive/togdrive.py
@@ -1,23 +1,25 @@
 
-#step 1 load the data into a dataframe
+#load the data into a dataframe
 import datetime, time
 import pandas as pd
 import pydrive 
 from google.cloud import storage
 
 tinker = 'TSLA'
+#here you can select time period
 period1 = int(time.mktime(datetime.datetime(2019, 7, 1, 23, 59).timetuple()))
 period2 = int(time.mktime(datetime.datetime(2019, 7, 31, 23, 59).timetuple()))
 interval = '1d' # 1wk, 1m
-query_string = f'C:/Users/hnrf/OneDrive/Документы/a CODE/01.2 data science analysis/TSLA.csv'
+#file location
+query_string = f'insert file location'  
 
 df = pd.read_csv(query_string)
 print(df)
 
 #input gcloud auth application-default login to login in vs/anyprogram to use gdrive with commands below
-#step 2 upload dataframe table to cloud storage
-client = storage.Client('C:/Users/hnrf/OneDrive/Документы/a CODE/01.3 data science Gdrive/client_secret_939665611566-niagp7rk4s4noc7undmuo6ejscaj2qhk.apps.googleusercontent.com.json')
-export_bucket = client.get_bucket('myd-storage-bucketmyd-storage-bucket') 
+#dataframe table to cloud storage
+client = storage.Client('insert location here') #google key/google json file location on pc
+export_bucket = client.get_bucket('insert correct name from gdrive') #correct google bucket
 
 df.to_csv()
 
